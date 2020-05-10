eff19aac2151819cd3cd11cf17b6526c1bd66175
Milos Bogosavljevic <milosnkbogosavljevic@gmail.com>
2020-05-10 03:01:11 +0200

    Sets up redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {createStore} from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
